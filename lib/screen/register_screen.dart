650e0806df372b7702139efdbd7557c4db2ddcd9
abishek <abishekkhanal2056@gmail.com>
2021-10-24 09:02:31 +0545

    Auth -- store user data, change in update and bug fixes

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:medicalapp/constants/styles.dart';
 import 'package:medicalapp/widgets/custom_text.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -12,27 +12,8 @@ class RegisterScreen extends StatelessWidget {
     String _email = "";
     String _password = "";
     String _fullName = "";
-    String _licensesNumber = "";
-    String _registeredDate = "";
 
-    void _dialogBox(String content) {
-      showDialog(
-          barrierDismissible: false,
-          context: context,
-          builder: (context) {
-            return CupertinoAlertDialog(
-              title: Text("Error"),
-              content: Text(content),
-              actions: [
-                ElevatedButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text("Close"))
-              ],
-            );
-          });
-    }
-
-    Future<void> _registerDoctor() async {
+    _registerDoctor() async {
       try {
         await FirebaseAuth.instance
             .createUserWithEmailAndPassword(email: _email, password: _password)
@@ -42,20 +23,17 @@ class RegisterScreen extends StatelessWidget {
                     .set({
                   "fullName": _fullName,
                   "email": _email,
-                  "password": _password,
                   "role": "doctor",
-                  "licensesNumber": _licensesNumber,
-                  "registeredDate": _registeredDate,
                 }));
-        Navigator.pushNamed(context, "/login");
+        Navigator.pushNamed(context, "/landing");
       } on FirebaseAuthException catch (e) {
         if (e.code == 'weak-password') {
-          _dialogBox('The password provided is too weak.');
+          print('The password provided is too weak.');
         } else if (e.code == 'email-already-in-use') {
-          _dialogBox('The account already exists for that email.');
+          print('The account already exists for that email.');
         }
       } catch (e) {
-        _dialogBox(e.toString());
+        print(e.toString());
       }
     }
 
@@ -63,95 +41,67 @@ class RegisterScreen extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: [
-            SizedBox(
-              height: 48.0,
-            ),
-            Center(
-              child: Text("Register as a Doctor"),
-            ),
-            SizedBox(
-              height: 32.0,
-            ),
             Container(
-              margin: EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  CustomTextField(
-                    helperText: "Full Name",
-                    hintText: 'Enter full name',
-                    obscureText: false,
-                    onChanged: (value) {
-                      _fullName = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  CustomTextField(
-                    helperText: "Email",
-                    hintText: 'Enter email address',
-                    obscureText: false,
-                    onChanged: (value) {
-                      _email = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  CustomTextField(
-                    helperText: "Password",
-                    hintText: 'Enter password',
-                    obscureText: true,
-                    onChanged: (value) {
-                      _password = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  CustomTextField(
-                    helperText: "Licenses number",
-                    hintText: 'Enter doctor licenses number',
-                    obscureText: false,
-                    onChanged: (value) {
-                      _licensesNumber = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  CustomTextField(
-                    helperText: "Registered Date",
-                    hintText: '2021-12-25',
-                    obscureText: false,
-                    onChanged: (value) {
-                      _registeredDate = value;
-                    },
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: RaisedButton(
-                      onPressed: (){
-                        _registerDoctor();
-
+              margin: EdgeInsets.only(top: 64.0),
+              child: Center(
+                child: Text(
+                  "Register as a Doctor",
+                  style: MyStyles.heading,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    CustomTextField(
+                      helperText: "Fullname",
+                      hintText: "Enter Fullname",
+                      onChanged: (value) {
+                        _fullName = value;
                       },
-                      child: Text("Register"),
+                      obscureText: false,
                     ),
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: ElevatedButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: Text("Back To Login"),
+                    SizedBox(
+                      height: 16.0,
                     ),
-                  ),
-                ],
+                    CustomTextField(
+                      helperText: "Email",
+                      hintText: "Enter email address",
+                      onChanged: (value) {
+                        _email = value;
+                      },
+                      obscureText: false,
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    CustomTextField(
+                      helperText: "Password",
+                      hintText: "Enter password",
+                      onChanged: (value) {
+                        _password = value;
+                      },
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _registerDoctor();
+                        },
+                        child: Text("Register"),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
