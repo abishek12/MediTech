c1aa127486909a090a3028a8313408ae3585096c
abishek <abishekkhanal2056@gmail.com>
2021-11-03 10:11:06 +0545

    bug fixes : 1.0.4

--- lib/screen/doctors_screen.dart
+++ lib/screen/doctors_screen.dart
@@ -1,71 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:medicalapp/screen/doctor_profile_screen.dart';
 import 'package:medicalapp/widgets/custom_appbar.dart';
 import 'package:medicalapp/widgets/custom_drawer.dart';
-import 'package:medicalapp/widgets/doctor_carousel.dart';
 
 class DoctorScreen extends StatelessWidget {
   const DoctorScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: myAppBar("Doctor"),
-      drawer: MyDrawer(),
-      body: ListView(
-        children: [
-          GestureDetector(
-            onTap: () => Navigator.pushNamed(context, "/doctorProfile"),
-            child: TopRatedDoctor(
-              imageName: "doctor1.jpg",
-              doctorName: "Ram Prasad Subedi",
-              doctorEmail: "ram@gmail.com",
-              likes: 500,
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+      stream: FirebaseFirestore.instance.collection('doctor').snapshots(),
+      builder: (_, snapshot) {
+        if (snapshot.hasError) return Text('Error = ${snapshot.error}');
+
+        if (snapshot.hasData) {
+          final docs = snapshot.data!.docs;
+          return Scaffold(
+            appBar: myAppBar("Doctor"),
+            drawer: MyDrawer(),
+            body: ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (_, i) {
+                final data = docs[i].data();
+                return Container(
+                  margin: EdgeInsets.all(16.0),
+                  child: Card(
+                    elevation: 2.0,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30.0,
+                        backgroundImage:
+                            AssetImage("assets/images/doctor1.jpg"),
+                      ),
+                      title: Text(data['fullName']),
+                      subtitle: Text(data['email']),
+                      trailing: IconButton(
+                        icon: Icon(CupertinoIcons.arrow_right_circle),
+                        onPressed: () =>
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => DoctorProfileScreen(
+                                      fullName: data['fullName'],
+                                      email: data['email'],
+                                      likes: data['likes'],
+                                      rating: data['rating'],
+                                      address: data['address'],
+                                      contact: data['contact'],
+                                      licensesNumber: data['licenses_number'],
+                                      description: data['description'],
+                                    ))),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-          TopRatedDoctor(
-            imageName: "doctor1.jpg",
-            doctorName: "Ram Subedi",
-            doctorEmail: "ram@gmail.com",
-            likes: 500,
-          ),
-          TopRatedDoctor(
-            imageName: "doctor1.jpg",
-            doctorName: "Ram Subedi",
-            doctorEmail: "ram@gmail.com",
-            likes: 500,
-          ),
-          TopRatedDoctor(
-            imageName: "doctor1.jpg",
-            doctorName: "Ram Subedi",
-            doctorEmail: "ram@gmail.com",
-            likes: 500,
-          ),
-          TopRatedDoctor(
-            imageName: "doctor1.jpg",
-            doctorName: "Ram Subedi",
-            doctorEmail: "ram@gmail.com",
-            likes: 500,
-          ),
-          TopRatedDoctor(
-            imageName: "doctor1.jpg",
-            doctorName: "Ram Subedi",
-            doctorEmail: "ram@gmail.com",
-            likes: 500,
-          ),
-          TopRatedDoctor(
-            imageName: "doctor1.jpg",
-            doctorName: "Ram Subedi",
-            doctorEmail: "ram@gmail.com",
-            likes: 500,
-          ),
-          TopRatedDoctor(
-            imageName: "doctor1.jpg",
-            doctorName: "Ram Subedi",
-            doctorEmail: "ram@gmail.com",
-            likes: 500,
-          ),
-        ],
-      ),
+          );
+        }
+
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
